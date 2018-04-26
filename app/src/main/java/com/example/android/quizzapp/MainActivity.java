4841a5d5c4b121dbea4d9f2a377cb03cdcd563fc
yvonnej <yvostel@gmail.com>
2018-04-26 10:27:45 +0200

    add resources

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.quizzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,7 +14,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private int points = 0;
-    private int questions = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,108 +22,107 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkAnswers(View view) {
-        Button btn = (Button)findViewById(R.id.submit_btn);
+        Button btn = findViewById(R.id.submit_btn);
         String btnText = btn.getText().toString();
-        if(!btnText.equals("Submit")) {
-            btn.setText("Submit");
+        if (!btnText.equals("Submit")) {
+            btn.setText(R.string.submit);
             points = 0;
             setContentView(R.layout.activity_main);
-        }else {
+        } else {
             // Check if all questions have an answer
-            EditText text = (EditText)findViewById(R.id.animal_name);
-            String animal = (String) text.getText().toString();
-            if(animal.isEmpty()) {
+            EditText text = findViewById(R.id.animal_name);
+            String animal = text.getText().toString();
+            if (animal.isEmpty()) {
                 Toast toast = Toast.makeText(getApplicationContext(), "You must answer all questions!", Toast.LENGTH_SHORT);
                 toast.show();
                 return;
             }
 
             // Check radio buttons for question 2
-            TextView viewAnswer = (TextView) findViewById(R.id.answer_textview_2);
+            TextView viewAnswer = findViewById(R.id.text_view_species_answer);
             String rightAnswer = viewAnswer.getText().toString();
             // Is the button now checked?
-            RadioGroup radiogroup1 = (RadioGroup)findViewById(R.id.radiogroup1);
-            if(radiogroup1.getCheckedRadioButtonId() == -1) {
+            RadioGroup radiogroup1 = findViewById(R.id.radiogroup_species);
+            if (radiogroup1.getCheckedRadioButtonId() == -1) {
                 Toast toast = Toast.makeText(getApplicationContext(), "You must answer all questions!", Toast.LENGTH_SHORT);
                 toast.show();
                 return;
             }
 
             // Check radio buttons for question 3 and if answer is right
-            TextView viewAnswer2 = (TextView) findViewById(R.id.answer_textview_3);
+            TextView viewAnswer2 = findViewById(R.id.text_view_giraffes_answer);
             String rightAnswer2 = viewAnswer2.getText().toString();
             // Is the button now checked?
-            RadioGroup radiogroup2 = (RadioGroup)findViewById(R.id.radiogroup2);
-            if(radiogroup2.getCheckedRadioButtonId() == -1) {
+            RadioGroup radiogroup2 = findViewById(R.id.radiogroup_giraffes);
+            if (radiogroup2.getCheckedRadioButtonId() == -1) {
                 Toast toast = Toast.makeText(getApplicationContext(), "You must answer all questions!", Toast.LENGTH_SHORT);
                 toast.show();
                 return;
             }
 
             // Which checkboxes are checked
-            CheckBox box1 = (CheckBox) findViewById(R.id.cbox1);
+            CheckBox box1 = findViewById(R.id.checbox_yak);
             boolean checkboxChecked1 = box1.isChecked();
-            CheckBox box2 = (CheckBox) findViewById(R.id.cbox2);
+            CheckBox box2 = findViewById(R.id.checkbox_parrot);
             boolean checkboxChecked2 = box2.isChecked();
-            CheckBox box3 = (CheckBox) findViewById(R.id.cbox3);
+            CheckBox box3 = findViewById(R.id.checkbox_okapi);
             boolean checkboxChecked3 = box3.isChecked();
-            CheckBox box4 = (CheckBox) findViewById(R.id.cbox4);
+            CheckBox box4 = findViewById(R.id.checkbox_sardines);
             boolean checkboxChecked4 = box4.isChecked();
 
-            if(!checkboxChecked1 && !checkboxChecked2 && !checkboxChecked3 && !checkboxChecked4) {
+            if (!checkboxChecked1 && !checkboxChecked2 && !checkboxChecked3 && !checkboxChecked4) {
                 Toast toast = Toast.makeText(getApplicationContext(), "You must answer all questions!", Toast.LENGTH_SHORT);
                 toast.show();
                 return;
             }
 
-
             // Question 1. Check if text is ok
             animal = animal.toLowerCase();
-            if(animal.equals("whale")) {
+            if (animal.equals("whale")) {
                 points++;
-                TextView tic = (TextView)findViewById(R.id.tic0);
+                TextView tic = findViewById(R.id.tic_animal);
                 tic.setVisibility(View.VISIBLE);
             }
 
             // Question 2. Get right answer
             int radioBtnId = radiogroup1.getCheckedRadioButtonId();
-            RadioButton btnChecked = (RadioButton)radiogroup1.findViewById(radioBtnId);
+            RadioButton btnChecked = radiogroup1.findViewById(radioBtnId);
             String selectedText = btnChecked.getText().toString();
-            if(rightAnswer.equals(selectedText)) {
+            if (rightAnswer.equals(selectedText)) {
                 points++;
-                TextView tic1 = (TextView)findViewById(R.id.tic1);
+                TextView tic1 = findViewById(R.id.tic_species);
                 tic1.setVisibility(View.VISIBLE);
             }
 
             // Question 3. Get right answer
             int radioBtnId2 = radiogroup2.getCheckedRadioButtonId();
-            RadioButton btnChecked2 = (RadioButton)radiogroup2.findViewById(radioBtnId2);
+            RadioButton btnChecked2 = radiogroup2.findViewById(radioBtnId2);
             String selectedText2 = btnChecked2.getText().toString();
-            if(rightAnswer2.equals(selectedText2)) {
+            if (rightAnswer2.equals(selectedText2)) {
                 points++;
-                TextView tic2 = (TextView)findViewById(R.id.tic2);
+                TextView tic2 = findViewById(R.id.tic_giraffes);
                 tic2.setVisibility(View.VISIBLE);
             }
 
-
             // Question 4. Check answer
-            if(checkboxChecked1 && checkboxChecked3 && !checkboxChecked2 && !checkboxChecked4) {
+            if (checkboxChecked1 && checkboxChecked3 && !checkboxChecked2 && !checkboxChecked4) {
                 points++;
-                TextView tic3 = (TextView)findViewById(R.id.tic3);
+                TextView tic3 = findViewById(R.id.tic_mammals);
                 tic3.setVisibility(View.VISIBLE);
             }
 
-            if(questions == points) {
+            // Number of questions in the quizz
+            int number_of_questions = 4;
+            if (number_of_questions == points) {
                 Toast toast = Toast.makeText(getApplicationContext(), "Greate all answers right!", Toast.LENGTH_SHORT);
                 toast.show();
-                btn.setText("Restart");
+                btn.setText(R.string.restart);
             } else {
                 Toast toast = Toast.makeText(getApplicationContext(), points + " answers right!", Toast.LENGTH_SHORT);
                 toast.show();
-                btn.setText("Try again");
+                btn.setText(R.string.try_again);
             }
             points = 0;
         }
-
     }
 }
